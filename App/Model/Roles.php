ea9d9a759128f8197ac902b4644810e1f1accd7f
PerrineTUB <117812132+PerrineTUB@users.noreply.github.com>
2023-03-21 16:25:17 +0100

    21/03/2023

---
+++ App/Model/Roles.php
@@ -0,0 +1,26 @@
+<?php
+
+    namespace App\Model;
+    use App\Utils\BddConnect;
+    class Roles extends BddConnect{
+        private $id_roles;
+        private $nom_roles;
+
+        public function __construct($name){
+            $this->id_roles = 1;
+            $this->nom_roles = $name;
+        }
+        
+        public function getIdRoles():?int{
+            return $this->id_roles;
+        }
+
+        public function getNomRoles():?string{
+            return $this->nom_roles;
+        }
+
+        public function setNomRoles($name):void{
+            $this->nom_roles = $name;
+        }
+    }
+?>
